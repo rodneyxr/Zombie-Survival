d88feac331c26413ea3d2b77e793dbe52a233b38
rodneyxr <rodneyxr@gmail.com>
2015-03-11 20:46:35 -0500

    Added main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public void StartGame() {
+        Application.LoadLevel("Level01");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+}
